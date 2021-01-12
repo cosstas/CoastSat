3faba622c2f0d779efae1bab1aed4c0d3ee178ed
Costas Yamin <costas.yamin@gmail.com>
2021-01-12 16:09:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,45 @@
 import setuptools
 
 setuptools.setup(
-    name='CoastSat',
+    name='coastsat-cosstas',
     version='1.2.0',
     packages=['coastsat'],
     url='https://github.com/kvos/CoastSat',
     license='GNU General Public License v3.0',
     author='Killian Vos',
     author_email='k.vos@unsw.edu.au',
-    description='A python toolkit to detect shoreline position from satellite imagery',
+    description='A python toolkit to obtain shoreline position detection from satellite imagery',
     long_description='CoastSat is an open-source software toolkit written in Python that enables users to obtain time-series of shoreline position at any coastline worldwide from 30+ years (and growing) of publicly available satellite imagery.',
-    packages=setuptools.find_packages(exclude=['gdal']),
+    long_description_content_type='text/markdown',
+    keywords='google-earth-engine, earth-engine, remote-sensing, satellite-images, coastal-engineering, shoreline-detection',
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Image Recognition"
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Natural Language :: English',
     ],
     python_requires='>=3.7',
-    install_requires = ['spyder,
-                        'notebook']
+    install_requires = [
+                        numpy=1.16.3,
+                        matplotlib=3.0.3,
+                        pillow=6.2.1,
+                        earthengine-api=0.1.236,
+                        # gdal=2.3.3,
+                        pandas=0.24.2,
+                        geopandas=0.4.1,
+                        pytz=2020.1,
+                        scikit-image=0.15.0,
+                        scikit-learn=0.20.3,
+                        shapely=1.6.4,
+                        scipy=1.2.1,
+                        astropy=3.2.1,
+                        spyder,
+                        notebook
+    ]
+    project_urls={
+                  'GDAL': 'https://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal'
+    },
 )
